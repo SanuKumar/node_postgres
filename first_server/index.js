2787dfbd323715965ce5a7791cd99ebaf0deec6c
sanukumar <sanukumar1138@gmail.com>
2023-01-04 00:52:32 +0530

    init fortunes api

--- first_server/index.js
+++ first_server/index.js
@@ -4,10 +4,24 @@ const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const { url } = req;
+
+  console.log(url)
+
+  if (url === '/translations') {
+    const translations = { 1: "one", 2: "two", 3: "three" }
+
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(translations));
+    return res.end()
+    // res.end('translations')
+  }
+
   res.end('Welcome to Node!')
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at ${hostname}:${port}`)
+  // console.log("Server running at " + hostname + ":" + port)
 })
 
